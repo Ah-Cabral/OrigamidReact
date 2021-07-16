88f8349ce716b07a6885dac8f1d469e1e52a36e6
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-15 23:18:41 -0300

    Desafio Components ReactJS

--- src/App.js
+++ src/App.js
@@ -1,52 +1,23 @@
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
+import React from 'react'
+import Header from "./Header";
+import Home from './Home';
+import Produtos from './Produtos';
 
 const App = () => {
+  let Pagina = Home;
+  const {pathname} = window.location;
+
+  if(pathname === '/produtos'){
+    Pagina = Produtos
+  }else{
+    Pagina = Home
+  }
   return (
     <section>
-      {produtos
-        .filter((produto) => Number(produto.preco.replace('R$ ', '')) > 1500)
-        .map((produto) => (
-          <div key={produto.id}>
-
-            <h1>{produto.nome}</h1>
-            <p>Preço: {produto.preco}</p>
-
-            <ul>
-
-              {produto.cores.map((cor) => (
-
-                <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
-                  {cor}
-                </li>
-                
-              ))}
-
-            </ul>
-          </div>
-        ))}
+      <Header/>
+      <Pagina/>
     </section>
   )
-};
+}
 
 export default App;
