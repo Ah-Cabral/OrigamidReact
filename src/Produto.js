d7e22e3a558682ff8e8d1c6f73885242d3b159a1
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-19 11:15:58 -0300

    Adding Verifications

--- src/Produto.js
+++ src/Produto.js
@@ -2,19 +2,21 @@ import React from 'react'
 
 const Produto = ({produto}) => {
     const [data, setData] = React.useState(null);
-
+    const [load, setLoad] = React.useState(null);
     React.useEffect(() =>{
+        setLoad(true)
         if(produto !== null){
             fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`).then(response => response.json()).then((json) => setData(json))
         }
+        setLoad(false)
     }, [produto])
 
     if(data === null) return null;
 
     return (
         <div>
-            <h1>{data.nome}</h1>
-            <h1>{data.preco}</h1>
+            {!load && data && <h1>{data.nome}</h1>}
+            {!load && data && <h1>{data.preco}</h1>}
         </div>
     )
 }
