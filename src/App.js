776deccba45e9282868276962d585e107fdd81e4
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-19 11:47:14 -0300

    use Ref - Parte 01

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import React from 'react'
-import Produto from './Produto'
 
 const App = () => {
+  const [comments, setComments] = React.useState(['1'])
+  const [input, setInput] = React.useState('1')
+  const inputElement = React.useRef()
 
-  // Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
-  // https://ranekapi.origamid.dev/json/api/produto/notebook
-  // https://ranekapi.origamid.dev/json/api/produto/smartphone
-  // Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
-  // Defina o produto clicado como uma preferência do usuário no localStorage
-  // Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
-
-  const [produto, setProduto] = React.useState(null)
-
-  React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem('Produto');
-    if (produtoLocal !== 'null') setProduto(produtoLocal);
-  }, []);
-
-  React.useEffect(() =>{
-    if(produto != null){
-      window.localStorage.setItem('Produto', produto);
-    };
-  }, [produto])
-
-  function handleClick({target}){
-    setProduto(target.innerText);
+  function handleClick(){
+    setComments([...comments, input])
+    setInput('')
+    inputElement.current.focus();
   }
 
   return (
     <div>
-      <h1>Preferência: {produto}</h1>
-      <button style={{margin: '1rem'}} onClick={handleClick}>notebook</button>
-      <button style={{margin: '1rem'}} onClick={handleClick}>smartphone</button>
-      <Produto produto={produto} />
+      <ul>
+        {comments.map(comment => <li key={comment}>{comment}</li>)}
+      </ul>
+
+      <input type='text' value={input} onChange={({target}) => setInput(target.value)}/>
+      <br/>
+      <button onClick={handleClick} ref={inputElement}>Enviar</button>
     </div>
   )
 }
