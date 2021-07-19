9267a64b7ab92f9011f5eb95d5710760407aa477
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-18 22:40:37 -0300

    Desafio useState()

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from 'react'
+import Produto from './Produto';
 
 const App = () => {
 
-  const [count, setCount] = React.useState(1);
-  const [items, setItems] = React.useState(['Item 1'])
+  // Os links abaixo puxam dados de um produto em formato JSON
+  // https://ranekapi.origamid.dev/json/api/produto/tablet
+  // https://ranekapi.origamid.dev/json/api/produto/smartphone
+  // https://ranekapi.origamid.dev/json/api/produto/notebook
+  // Crie uma interface com 3 botões, um para cada produto.
+  // Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+  // Mostre apenas um produto por vez
+  // Mostre a mensagem carregando... enquanto o fetch é realizado
 
-  function handleClick(){
-    setCount((count )=> count + 1)
-    setItems((items)=> [...items, 'Item ' + (count + 1)]);
+  //Primeiro de tudo, se é um dado que é modificado, devemos utilizar o useState
+
+  const [data, setData] = React.useState(null);
+  const [load, setLoad] = React.useState(null);
+
+  async function handleClick(event){
+    setLoad(true)
+    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
+    const json = await response.json()
+    setData(json)
+    setLoad(false)
   }
 
   return (
     <div>
-      {items.map(item => <li key={item}>{item}</li>)}
-      <button onClick={handleClick}> {count} </button>
+      <button onClick={handleClick} style={{margin: '5px'}}>notebook</button>
+      <button onClick={handleClick} style={{margin: '5px'}}>smartphone</button>
+      <button onClick={handleClick} style={{margin: '5px'}}>tablet</button>
+      {load && <p>Carregando...</p>}
+      {!load && data && <Produto data={data}/>}
     </div>
   )
 }
